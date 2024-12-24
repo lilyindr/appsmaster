d211b0aee720ef8dbe4ceb28093da620f40d1828
lilyindr <lily@ppu.co.id>
2024-12-24 15:54:51 +0700

    first time

---
+++ src/main/java/com/appsmaster/AppsmasterApplication.java
@@ -0,0 +1,13 @@
+package com.appsmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsmasterApplication.class, args);
+	}
+
+}
