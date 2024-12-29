ca2822269f1629d918260f225592f00e7b890b5b
andirhl13 <lily.indriana@gmail.com>
2024-12-29 10:50:24 +0700

    update andi rhl 29/12/2024 coba push

---
+++ src/main/java/com/appsmaster/Repository/ICoreMstAdminStatusRepository.java
@@ -0,0 +1,10 @@
+package com.appsmaster.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.appsmaster.Models.CoreMstAdminStatus;
+
+
+public interface ICoreMstAdminStatusRepository extends JpaRepository<CoreMstAdminStatus,Integer>{
+
+}
