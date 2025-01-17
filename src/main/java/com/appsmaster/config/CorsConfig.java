c355b605bbcf6e9afcc53f54955b08b27fe442e2
andlimhot <lily.indriana@gmail.com>
2025-01-17 19:22:43 +0700

    update 20250117 - by am

--- src/main/java/com/appsmaster/config/CorsConfig.java
+++ src/main/java/com/appsmaster/config/CorsConfig.java
@@ -1,11 +1,16 @@
 package com.appsmaster.config;
 
 
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import jakarta.servlet.MultipartConfigElement;
+
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.unit.DataSize;
 
 
 
@@ -24,5 +29,13 @@ public class CorsConfig  {
 
            }
        };
+   }
+   
+   @Bean
+   MultipartConfigElement multipartConfigElement() {
+       MultipartConfigFactory factory = new MultipartConfigFactory();
+       factory.setMaxFileSize(DataSize.ofMegabytes(10)); // Sesuaikan ukuran maksimum file
+       factory.setMaxRequestSize(DataSize.ofMegabytes(20)); // Sesuaikan ukuran maksimum request
+       return factory.createMultipartConfig();
    }
 }
